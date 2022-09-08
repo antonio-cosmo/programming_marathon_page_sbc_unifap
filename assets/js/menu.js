ef55242b46f017439ffe1578938edfce54139e8c
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2022-09-08 15:16:31 -0300

    modified function closed menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,11 @@
-const links = document.querySelectorAll(".link-item");
-const checkebox = document.querySelector("#checkbox-menu")
-const btnTopPage = document.querySelector('.top-page')
-let lastScrollPosition = 0
-let positionBtnTopPage = btnTopPage.getBoundingClientRect()
-for (i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function() {
-        checkebox.checked = false
-    });
-}
+(()=> {
+    const links = document.querySelectorAll(".link-item");
+    const checkebox = document.querySelector("#checkbox-menu")
+
+    for (i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function() {
+            checkebox.checked = false
+        });
+    }
+
+})();
