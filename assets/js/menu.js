dc7014f5084dc7e0287aeccf20af2bee4531eeb0
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2022-09-11 19:29:52 -0300

    modifications-02

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,17 @@
 (()=> {
     const links = document.querySelectorAll(".link-item");
     const checkebox = document.querySelector("#checkbox-menu")
-
+    const container  = document.querySelector('.container')
     for (i = 0; i < links.length; i++) {
         links[i].addEventListener("click", function() {
             checkebox.checked = false
         });
     }
 
+    window.addEventListener('touchstart',(e)=>{
+        const el = e.target
+        if(!el.classList.contains('navbar-mobile') && !el.classList.contains('link-item')){
+            checkebox.checked = false
+        }
+    })
 })();
