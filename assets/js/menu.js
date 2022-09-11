4441cd68642740ba5adbe3274491ae6e97585932
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2022-09-11 20:41:08 -0300

    add favicon

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,28 @@
 (()=> {
     const links = document.querySelectorAll(".link-item");
     const checkebox = document.querySelector("#checkbox-menu")
-    const container  = document.querySelector('.container')
+    const toggleMenu = document.querySelector('.checkbox')
     for (i = 0; i < links.length; i++) {
         links[i].addEventListener("click", function() {
             checkebox.checked = false
         });
     }
 
-    window.addEventListener('touchstart',(e)=>{
-        const el = e.target
-        if(!el.classList.contains('navbar-mobile') && !el.classList.contains('link-item')){
-            checkebox.checked = false
-        }
-    })
+    if ('ontouchstart' in window) {
+        window.addEventListener('touchstart',(e)=>{
+            const el = e.target
+    
+            if(el.classList.contains('checkbox')){
+                checkebox.checked = !checkebox.checked
+            }
+            else if(!el.classList.contains('navbar-mobile') && !el.classList.contains('link-item')){
+                checkebox.checked = false
+            }
+    
+        })
+    }else{
+        toggleMenu.addEventListener('click', ()=>{
+            checkebox.checked = !checkebox.checked 
+        })
+    }
 })();
